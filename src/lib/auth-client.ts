d5ac0a4604632847afac0fac6b63427496bbfb57
Jean Didier <tarpagajd@gmail.com>
2025-12-09 23:21:49 +0000

    fix env var reference name

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
 
