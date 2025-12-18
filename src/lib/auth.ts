b3d62b21a1ccf46d85d2ca45a8e56b531a1afbbe
Jean Didier <tarpagajd@gmail.com>
2025-12-18 04:58:28 +0000

    fix: Set input to false for role field to prevent security vulnerabilities

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -33,6 +33,7 @@ export const auth = betterAuth({
       role: {
         type: "string",
         defaultValue: "user",
+        input: false, //! critically important : https://www.better-auth.com/docs/concepts/typescript#inferring-types:~:text=By%20default%2C%20additional%20fields%20are%20included%20in%20the%20user%20input%2C%20which%20can%20lead%20to%20security%20vulnerabilities%20if%20not%20handled%20carefully.%20For%20fields%20that%20should%20not%20be%20set%20by%20the%20user%2C%20like%20a%20role%2C%20it%20is%20crucial%20to%20set%20input%3A%20false%20in%20the%20configuration.
       },
     },
   },
